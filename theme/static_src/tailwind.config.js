e1264e2243fe28e3c015d3b84bc62bb824f2489e
Joaquin Anabalon <joaquin.anabalon@inacapmail.cl>
2024-02-09 21:40:37 -0300

    Añadir estilos de navbar y controlador de tema

--- theme/static_src/tailwind.config.js
+++ theme/static_src/tailwind.config.js
@@ -55,5 +55,6 @@ module.exports = {
         require('@tailwindcss/forms'),
         require('@tailwindcss/typography'),
         require('@tailwindcss/aspect-ratio'),
+        require('@tailwindcss/aspect-ratio'),
     ],
 }
